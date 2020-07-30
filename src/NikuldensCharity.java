55a1a9c1d3534f9112201b38e9d4129d366fdee7
Mihail Hristov <mis6us@icloud.com>
2020-07-30 09:40:39 +0300

    Add some code

--- src/NikuldensCharity.java
+++ src/NikuldensCharity.java
@@ -4,5 +4,24 @@ public class NikuldensCharity {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        String inputtedString = scanner.next();
+
+        String input = scanner.nextLine();
+        while (!"Finish".equals(input)){
+            String[] tokens = input.split("\\s+");
+            String command = tokens[0];
+
+            switch (command){
+                case "Replace" : break;
+                case "Cut" : break;
+                case "Make" : break;
+                case "Check" : break;
+                case "Sum" : break;
+
+            }
+
+
+            input = scanner.nextLine();
+        }
     }
 }
