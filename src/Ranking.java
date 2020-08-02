dce31177eced39b44741e99cb5cb3fce6f88a073
Mihail Hristov <mis6us@icloud.com>
2020-08-02 20:12:35 +0300

    add all src code

--- src/Ranking.java
+++ src/Ranking.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
 public class Ranking {
     public static void main(String[] args) {
@@ -6,6 +7,9 @@ public class Ranking {
 
         Map<String, String> contents = new HashMap<>();
         Map<String, Map<String, Integer>> submissions = new TreeMap<>();
+
+        String userWithMostPoints = "";
+        int mostPointForUser = 0;
 
         String input = scanner.nextLine();
 
@@ -29,11 +33,38 @@ public class Ranking {
             int points = Integer.parseInt(tokens[3]);
 
             if (contents.containsKey(contest) && contents.get(contest).equals(password)){
-
+                submissions.putIfAbsent(username, new HashMap<>());
+                submissions.get(username).putIfAbsent(contest, 0);
+                if (submissions.get(username).get(contest) < points) {
+                    submissions.get(username).put(contest, points);
+                }
             }
 
             secondInput = scanner.nextLine();
         }
+
+        for (Map.Entry<String, Map<String, Integer>> user : submissions.entrySet()) {
+            int currentSum = user.getValue().values().stream().mapToInt(i -> i).sum();
+            if (currentSum > mostPointForUser){
+                mostPointForUser = currentSum;
+                userWithMostPoints = user.getKey();
+            }
+        }
+
+        System.out.println(String.format("Best candidate is %s with total %d points.",userWithMostPoints, mostPointForUser));
+        System.out.println("Ranking: ");
+
+        submissions
+                .forEach((key, value) -> {
+                    System.out.println(key);
+                    submissions.get(key)
+                            .entrySet()
+                            .stream()
+                            .sorted((s1, s2) -> Integer.compare(s2.getValue(), s1.getValue()))
+                            .forEach((s) -> {
+                                System.out.println(String.format("#  %s -> %d", s.getKey(), s.getValue()));
+                            });
+                });
 
     }
 }
