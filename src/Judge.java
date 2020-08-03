fd1050f6b48a731ffda6f51ba1c08b839bdce1dc
Mihail Hristov <mis6us@icloud.com>
2020-08-03 22:23:43 +0300

    add while and tokens for each line

--- src/Judge.java
+++ src/Judge.java
@@ -1,10 +1,25 @@
+import java.awt.*;
+import java.util.LinkedHashMap;
+import java.util.Map;
 import java.util.Scanner;
 
 public class Judge {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
+        Map<String, Map<String, Integer>> contests = new LinkedHashMap<>();
+
+        String input = scanner.nextLine();
+        while (!"no more time".equals(input)){
+            String[] tokens = input.split("->");
+            String username = tokens[0];
+            String contest = tokens[1];
+            int points = Integer.parseInt(tokens[2]);
 
 
+
+
+            input = scanner.nextLine();
+        }
     }
 }
